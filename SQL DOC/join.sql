1ba0a3b928c4b09e79c320482ef0ff5d06733acc
Danangadi26 <zeroonex72017@gmail.com>
2024-10-11 11:18:20 +0700

    update

--- SQL DOC/join.sql
+++ SQL DOC/join.sql
@@ -1,138 +1,3 @@
--- Count (total seluruh data)
-SELECT COUNT(field_name) AS name_alias
-FROM name_table;
-
--- Count multiple fields
-SELECT COUNT(field_name) AS name_alias, COUNT(field_name) AS name_alias 
-FROM name_table;
-
--- Count all fields
-SELECT COUNT(*) AS name_alias
-FROM name_table;
-
--- Distinct (menghapus seluruh data duplikat)
-SELECT DISTINCT field_name
-FROM name_table;
-
--- Count with Distinct (total seluruh nilai tanpa nilai duplikat)
-SELECT COUNT(DISTINCT field_name) AS name_alias
-FROM name_table;
-
--- LIMIT (membatasi query view/output)
-SELECT field_name 
-FROM name_table
-LIMIT number_limit;
-
--- WHERE (mencari letak query)
-SELECT field_name
-FROM name_table
-WHERE field_name = 'value';
-
--- WHERE with comparison operators (>, <, >=, <=) noted (<> artinya !=)
-SELECT field_name
-FROM name_table
-WHERE field_name > 'value';
-
--- WHERE multiple criteria OR, AND, BETWEEN 
-SELECT field_name
-FROM name_table
-WHERE field_name = 'value' OR field_name = 'value';
-
--- WHERE OR, AND operator
-SELECT field_name
-FROM name_table
-WHERE field_name = 'value'
-  OR field_name = 'value';
-
--- WHERE AND, OR
-SELECT field_name
-FROM name_table
-WHERE (field_name = 'value' OR field_name = 'value') 
-  AND (field_name = 'value' OR field_name = 'value');
-
--- WHERE BETWEEN, AND
-SELECT field_name
-FROM name_table
-WHERE field_name 
-  BETWEEN value1 AND value2;
-
--- WHERE BETWEEN, AND, OR
-SELECT field_name
-FROM name_table
-WHERE field_name 
-  BETWEEN value1 AND value2 AND field_name = 'value';
-
--- LIKE Query 
-SELECT field_name 
-FROM name_table
-WHERE field_name LIKE 'value%' -- value mencari value.....
-WHERE field_name LIKE '__value%' -- value mencari karakter value ketiga.....
-WHERE field_name LIKE '%value'; -- value mencari .....value
-
--- NOT LIKE Query 
-SELECT field_name 
-FROM name_table
-WHERE field_name NOT LIKE 'value%' -- value mencari kecuali value.....
-WHERE field_name NOT LIKE '%value'; -- value mencari kecuali .....value
-
--- WHERE, IN
-SELECT field_name
-FROM name_table
-WHERE field_name IN (value1, value2, value3);
-
--- NULL values
-SELECT field_name
-FROM name_table
-WHERE field_name IS NULL; -- Include nilai null
-WHERE field_name IS NOT NULL; -- Not include nilai null
-
--- Aggregate function (AVG(), SUM(), MIN(), MAX(), COUNT())
-SELECT Aggregate_function(field_name) AS name_alias
-FROM name_table;
-
--- Aggregate function with WHERE Condition
-SELECT Aggregate_function(field_name) AS name_alias
-FROM name_table
-WHERE field_name >= value;
-
--- ROUND() = number specified decimal
-SELECT ROUND(AVG(field_name), number_places_after_coma) AS name_alias
-FROM name_table
-WHERE field_name >= value;
-
--- No number after decimal
-SELECT ROUND(AVG(field_name)) AS name_alias
-FROM name_table
-WHERE field_name >= value;
-
--- Aliasing with arithmatic (+, -, /, *)
-SELECT (field_name - field_name) AS name_alias
-FROM name_table;
-
--- ORDER BY (ASC = Up, DESC = Down)
-SELECT field_name, field_name
-FROM name_table
-ORDER BY field_name;
-
-SELECT field_name, field_name
-FROM name_table
-ORDER BY field_name ASC, field_name DESC;
-
--- Group BY single field
-SELECT field_name, COUNT(field_name) AS name_alias
-FROM name_table
-GROUP BY field_name, field_name
-ORDER BY field_name ASC, field_name DESC
-LIMIT number_limit;
-
--- Filtering Grouped data
-SELECT
-    field_name
-    COUNT(field_name) AS name_alias
-FROM name_table
-GROUP BY field_name
-HAVING COUNT(field_name) > number_filtered;
-
 -- Inner JOIN
 SELECT name_table.field_name, field_name, field_name -- Noted : table.field_name digunakan ketika field_name terdapat dalam 2 table
 FROM name_table -- table wadah 
@@ -163,7 +28,7 @@ SELECT *
 FROM left_table AS l 
 INNER JOIN right_table AS r 
 ON left_table_id = right_table_id
-  AND left_table_date = right_table_date
+    AND left_table_date = right_table_date
 
 -- Left Join 
 SELECT 
@@ -224,7 +89,6 @@ WHERE p1.year = 2010
 -- CROSS = Mengembalikan seluruh baris dari table dengan berbagai kondisi.
 -- SELF = Mengembalikan seluruh baris dari table untuk dirinya sendiri.
 
-
 -- NOTED UNION and UNION ALL = mengembalikan nilai dari kedua table, UNION ALL dia akan mengembalikan nilai walaupun duplikat
 SELECT * 
 FROM left_table
@@ -270,10 +134,10 @@ ORDER BY name;
 SELECT president, country, continent 
 FROM president 
 WHERE country IN
-  (
-    SELECT country 
-    FROM states
-    WHERE indep_year = 1800); 
+    (
+        SELECT country 
+        FROM states
+        WHERE indep_year = 1800); 
 
 -- ANTI JOIN = Mengembalikan nilai table kiri yang tidak memiliki duplikat di table kanan
 SELECT DISTINCT name -- Output hanya menghasilkan bahasa yang berada di country 'middle east'
@@ -287,44 +151,10 @@ ORDER BY name;
 SELECT code, name
 FROM countries
 WHERE continent = 'Oceania'
-  AND code NOT IN
-    (SELECT code
-    FROM currencies);
+    AND code NOT IN
+        (SELECT code
+        FROM currencies);
 
--- Subquery = Menampilkan hasil dengan kondisi pada table lain nya
-SELECT name, country_code, urbanarea_pop
-FROM cities
-WHERE name IN 
-    (
-        SELECT capital
-        FROM countries)
-ORDER BY urbanarea_pop DESC;
-
--- Subquery in SELECT
-SELECT countries.name AS country,  
-  (SELECT COUNT(*) AS cities_num
-  FROM cities
-  WHERE cities.country_code = countries.code) AS cities_num
-FROM countries
-ORDER BY cities_num DESC, country
-LIMIT 9;
-
--- Subquery in FROM
-SELECT DISTINCT monarch.continent, sub.most_recent 
-FROM monarch, 
-  (SELECT continent, MAX(indep_year) AS most_recent
-  FROM states 
-  GROUP BY continent) AS sub
-WHERE monarch.continent = sub.continent
-ORDER BY continent;
-
-SELECT local_name, sub.lang_num
-FROM countries,
-  (SELECT code, COUNT(*) AS lang_num
-    FROM languages
-    GROUP BY code) AS sub
-WHERE countries.code = sub.code
-ORDER BY lang_num DESC;
 
 -- Final JOIN Project
 SELECT 
