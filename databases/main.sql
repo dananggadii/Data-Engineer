d4f43c3930b8dc90b0cde589dca1dd86f95a6cce
Danangadi26 <zeroonex72017@gmail.com>
2024-10-13 11:14:03 +0700

    update

--- databases/main.sql
+++ databases/main.sql
@@ -14,23 +14,46 @@ CREATE TABLE table_name (
   column_b data_type,
   column_c data_type,
 );
+
 -- Add Column to table from databases 
 ALTER TABLE table_name
 ADD COLUMN column_name data_type;
 
--- Example 
+-- Count and Distinct for sum total data with no duplicate values 
+SELECT COUNT(*)
+FROM column_name;
+-- DISTINCT
+SELECT COUNT(DISTINCT table_name)
+FROM column_name;
+
+-- Insert or add data from table_name to another_table with unique values/Distinct (no duplicate values)
+INSERT INTO another_table
+SELECT DISTINCT column_name, column_name
+FROM table_name;
+
+-- Insert Into statement or normal 
+INSERT INTO table_name (column_a, column_b)
+VALUES ('value_a', 'value_b');
+
+-- Rename Column_name
+ALTER TABLE table_name
+RENAME COLUMN old_name TO new_name;
+
+----------------- Example -----------------------------
+
 -- Create a table for the professors entity type
 CREATE TABLE professors (
   firstname text,
   lastname text
 );
+
 -- Alter table for the professors to add new column 
 ALTER TABLE professors
 ADD COLUMN university_shortname text;
+
 -- Print the contents of this table
 SELECT * 
 FROM professors;
-
 
 
 
