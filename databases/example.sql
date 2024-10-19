8065688b2f6156afaa788724abac3dd2fa0a827f
Danangadi26 <zeroonex72017@gmail.com>
2024-10-19 16:04:07 +0700

    update

--- databases/example.sql
+++ databases/example.sql
@@ -36,6 +36,7 @@ FROM professors;
 -- Delete the university_professors table
 DROP TABLE university_professors;
 
+------------------------------------------------------------------------------------------------------------------------------
 
 -- CREATE table with multiple data types 
 CREATE TABLE weather (
@@ -46,4 +47,31 @@ CREATE TABLE weather (
 -- Convert data types from text to integer 
 SELECT temperature * CAST(wind_speed AS integer) AS wind_chill 
 FROM weather;
+
+------------------------------------------------------------------------------------------------------------------------------
+
+CREATE TABLE students (
+  ssn integer,
+  name varchar(64),
+  dob date,
+  average_grade numeric(3,2),
+  tuition_paid boolean
+);
+
+-- ALTER TABLE AFTER CREATION 
+ALTER TABLE students 
+ALTER COLUMN name
+TYPE varchar(128);
+
+ALTER TABLE students
+ALTER COLUMN average_grade 
+TYPE integer 
+-- Turns 5.54 into 8, not 5 before type conversion
+USING ROUND(average_grade);
+
+-- SUBSTRING = Make sure values in name column to fit the new maximum length of 16 characters 
+ALTER TABLE students 
+ALTER COLUMN name 
+TYPE varchar(16) 
+USING SUBSTRING(name FROM 1 FOR 16);
 
