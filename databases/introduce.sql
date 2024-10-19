b3415dd1aa6a55222e54c6cf2fd4e231cb795cc2
Danangadi26 <zeroonex72017@gmail.com>
2024-10-19 22:10:25 +0700

    update

--- databases/introduce.sql
+++ databases/introduce.sql
@@ -208,3 +208,41 @@ SELECT * FROM cars;
 
 ------------------------------------------------------------------------------------------------------------------------------
 
+-- Foreign keys
+CREATE TABLE manufactures (
+  name varchar(255) PRIMARY KEY
+);
+
+INSERT INTO manufactures 
+VALUES ('bmw'), ('lamborgini'), ('mercendez');
+
+CREATE TABLE cars (
+  model varchar(255) PRIMARY KEY,
+  manufacture_name varchar(255) REFERENCES manufactures(name)
+);
+
+INSERT INTO cars 
+VALUES ('Ranger', 'Ford'), ('Bettle', 'VW');
+
+------------------------------------------------------------------------------------------------------------------------------
+
+-- Rename the university_shortname column
+ALTER TABLE professors
+RENAME COLUMN university_shortname TO university_id;
+
+-- Add a foreign key on professors referencing universities
+ALTER TABLE professors 
+ADD CONSTRAINT professors_fkey FOREIGN KEY (university_id) REFERENCES universities (id);
+
+------------------------------------------------------------------------------------------------------------------------------
+
+-- Foreign Key Use Case
+-- Select all professors working for universities in the city of Zurich
+SELECT professors.lastname, universities.id, universities.university_city
+FROM professors
+JOIN universities
+ON professors.university_id = universities.id
+WHERE universities.university_city = 'Zurich';
+
+------------------------------------------------------------------------------------------------------------------------------
+
