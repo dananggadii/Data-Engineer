9f187284562481a52a03634a1dde6ec1ae0f09f3
Danangadi26 <zeroonex72017@gmail.com>
2024-10-19 19:10:42 +0700

    update

--- databases/introduce.sql
+++ databases/introduce.sql
@@ -147,6 +147,15 @@ DROP NOT Null;
 
 ------------------------------------------------------------------------------------------------------------------------------
 
+-- Create the table
+CREATE TABLE students (
+  last_name varchar(128) NOT NULL,
+  ssn integer PRIMARY KEY,
+  phone_no char(12)
+);
+
+------------------------------------------------------------------------------------------------------------------------------
+
 -- Unique Constraint to Column
 ALTER TABLE universities
 ADD CONSTRAINT university_shortname_unq UNIQUE(university_shortname); -- create a new column with unique values from the university_shortname 
@@ -160,3 +169,42 @@ RENAME COLUMN organization TO id;
 -- Make id a primary key
 ALTER TABLE organizations
 ADD CONSTRAINT organization_pk PRIMARY KEY (id);
+
+------------------------------------------------------------------------------------------------------------------------------
+
+-- Add the new column to the table
+ALTER TABLE professors 
+ADD COLUMN id serial;
+
+-- Make id a primary key
+ALTER TABLE professors 
+ADD CONSTRAINT professors_pkey PRIMARY KEY (id);
+
+-- Have a look at the first 10 rows of professors
+SELECT * 
+FROM professors
+LIMIT 10;
+
+------------------------------------------------------------------------------------------------------------------------------
+
+-- Count the number of distinct rows with columns make, model
+SELECT COUNT(DISTINCT(make, model)) 
+FROM cars;
+
+-- Add the id column
+ALTER TABLE cars
+ADD COLUMN id varchar(128);
+
+-- Update id with make + model
+UPDATE cars
+SET id = CONCAT(make, model);
+
+-- Make id a primary key
+ALTER TABLE cars
+ADD CONSTRAINT id_pk PRIMARY KEY(id);
+
+-- Have a look at the table
+SELECT * FROM cars;
+
+------------------------------------------------------------------------------------------------------------------------------
+
